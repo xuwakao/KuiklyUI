cc331da63884765d3f6392b027e22d3059fa433c
xuwakao <xuwakao@gmail.com>
2026-03-25 13:27:09 +0800

    chore: upgrade JVM target from 1.8 to 21 for compose and core-render-android

--- compose/build.2.1.21.gradle.kts
+++ compose/build.2.1.21.gradle.kts
@@ -15,7 +15,7 @@ kotlin {
     androidTarget {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "1.8"
+                jvmTarget = "21"
                 freeCompilerArgs += "-Xjvm-default=all"
                 moduleName = "${project.group}.${project.name}"
             }
@@ -129,7 +129,7 @@ android {
         minSdk = 21
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
